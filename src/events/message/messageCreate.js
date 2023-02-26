6ae5e05b07c0dd86d159698dc6e5765fd88d2bb6
N0rule <N0rule228@gmail.com>
2023-02-26 17:04:53 +0200

    const eror fix,+ optimization

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -1,7 +1,7 @@
 const { commandHandler, automodHandler, statsHandler } = require("@src/handlers");
-const { EMBED_COLORS,PREFIX_COMMANDS } = require("@root/config");
+const { EMBED_COLORS, PREFIX_COMMANDS } = require("@root/config");
 const { getSettings } = require("@schemas/Guild");
-const {EmbedBuilder} = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 const { text } = require("stream/consumers");
 /**
  * @param {import('@src/structures').BotClient} client
@@ -12,21 +12,17 @@ const { text } = require("stream/consumers");
 module.exports = async (client, message, guild) => {
   if (!message.guild || message.author.bot) return;
   const settings = await getSettings(message.guild);
-  const desc = `Приветики я **${message.guild.members.me.displayName}!**\n`;
-  desc += `Личный Бот Синдиката 🥰\n`;
-  desc += `Мой Префикс \`${settings.prefix}\`\n`;
-  desc += `Для помощи используй команду **/help**\n`;
-
   const embed = new EmbedBuilder()
-  .setColor(EMBED_COLORS.BOT_EMBED)
-  .setThumbnail(client.user.displayAvatarURL())
-  .setDescription(desc);
+    .setColor(EMBED_COLORS.BOT_EMBED)
+    .setThumbnail(client.user.displayAvatarURL())
+    .setDescription(`Приветики я **${message.guild.members.me.displayName}!**\nЛичный Бот Синдиката 🥰\nМой Префикс \`${settings.prefix}\`\nДля помощи используй команду **/help**\n`);
   // command handler
   let isCommand = false;
   if (PREFIX_COMMANDS.ENABLED) {
     // check for bot mentions
     if (message.content.includes(`${client.user.id}`)) {
-      message.channel.safeSend({embeds: [embed]})}
+      message.channel.safeSend({ embeds: [embed] })
+    }
 
     if (message.content && message.content.startsWith(settings.prefix)) {
       const invoke = message.content.replace(`${settings.prefix}`, "").split(/\s+/)[0];
