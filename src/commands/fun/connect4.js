e3dab0715aece1c8ce2627ab15922b9e79899e70
N0rule <N0rule228@gmail.com>
2023-04-16 16:33:14 +0300

    added 2 ne w games, fixes

--- src/commands/fun/connect4.js
+++ src/commands/fun/connect4.js
@@ -1,4 +1,4 @@
-const { TicTacToe } = require("discord-gamecord");
+const { Connect4 } = require("discord-gamecord");
 const { ApplicationCommandOptionType, EmbedBuilder } = require("discord.js");
 
 /**
@@ -6,8 +6,8 @@ const { ApplicationCommandOptionType, EmbedBuilder } = require("discord.js");
  */
 
 module.exports = {
-  name: "tictactoe",
-  description: "Играйте в Крестики Нолики с друзьями",
+  name: "connect4",
+  description: "Играйте в Connect4 с друзьями",
   cooldown: 40,
   category: "FUN",
   botPermissions: ["SendMessages", "EmbedLinks", "AddReactions", "ReadMessageHistory", "ManageMessages"],
@@ -28,12 +28,12 @@ module.exports = {
   },
 
   async interactionRun(interaction) {
-    const Game = new TicTacToe({
+    const Game = new Connect4({
       message: interaction,
       isSlashGame: true,
       opponent: interaction.options.getUser("user"),
       embed: {
-        title: "Крестики Нолики",
+        title: "Connect4",
         color: "#5865F2",
         statusTitle: "Статус",
         overTitle: "Конец Игры",
@@ -48,12 +48,12 @@ module.exports = {
       xButtonStyle: "DANGER",
       oButtonStyle: "PRIMARY",
       turnMessage: "{emoji} | Очередь игрока **{player}**.",
-      winMessage: "{emoji} | **{player}** выиграл в Крестики Нолики.",
+      winMessage: "{emoji} | **{player}** выиграл в Connect4.",
       tieMessage: "Ничья!Никто не выиграл игру!",
       timeoutMessage: "Игра не закончилась!Никто не выиграл игру!",
       playerOnlyMessage: "Только {player} и {opponent} могут использовать эти кнопки.",
-      requestMessage: "{player} пригласил вас на раунд в **Крестики Нолики**.",
-      rejectMessage: "Игрок отклонил ваш запрос на раунд в **Крестики Нолики**.",
+      requestMessage: "{player} пригласил вас на раунд в **Connect4**.",
+      rejectMessage: "Игрок отклонил ваш запрос на раунд в **Connect4**.",
     });
 
     Game.startGame();
@@ -63,15 +63,15 @@ module.exports = {
       const winner = `<@${winners}>`;
       if (result.result === "tie") {
         const embed = new EmbedBuilder()
-          .setTitle("Крестики Нолики")
+          .setTitle("Connect4")
           .setDescription("Ничья! Никто не выиграл игру!")
           .setColor("Red")
           .setTimestamp();
         interaction.followUp({ embeds: [embed] });
       } else if (result.result === "win") {
         const embed = new EmbedBuilder()
-          .setTitle("Крестики Нолики")
-          .setDescription(`${winner} выиграл в Крестики Нолики.`)
+          .setTitle("Connect4")
+          .setDescription(`${winner} выиграл в Connect4.`)
           .setColor("Green")
           .setTimestamp();
 
---
+++ src/commands/fun/guessThePokemon.js
@@ -0,0 +1,38 @@
+const { GuessThePokemon } = require("discord-gamecord");
+
+/**
+ * @type {import("@structures/Command")}
+ */
+
+module.exports = {
+    name: "pokemonguesser",
+    description: "Играть в угадай покемона",
+    cooldown: 15,
+    category: "FUN",
+    botPermissions: ["SendMessages", "EmbedLinks", "AddReactions", "ReadMessageHistory", "ManageMessages"],
+    command: {
+        enabled: false,
+    },
+    slashCommand: {
+        enabled: true,
+        ephermal: true,
+        options: [],
+    },
+
+    async interactionRun(interaction) {
+        const Game = new GuessThePokemon({
+            message: interaction,
+            isSlashGame: true,
+            embed: {
+                title: "Это что за покемон?",
+                color: "#5865F2",
+            },
+            stopButton: "Прекратить",
+            winMessage: "Вы догадались правильно!Это был **{pokemon}**.",
+            loseMessage: "Повезет в следующий раз!Это был **{pokemon}**.",
+            errMessage: "🚫 Невозможно получить данные Покемонов! Пожалуйста, попробуйте еще раз.",
+        });
+
+        Game.startGame();
+    },
+};
